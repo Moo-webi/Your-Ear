778dd310f3ada5e0bff4d931056938603a5d487f
muhammed <mo23mabrouk@gmail.com>
2025-07-15 00:42:33 +0100

    initializing the backend

---
+++ backend/your_ear/your_ears/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YourEarsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'your_ears'
---
+++ backend/your_ear/your_ears/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('example/', views.example_view, name='example'),
+]
